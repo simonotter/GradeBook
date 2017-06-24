0e9196adb80204df66ad491732e9f889e0edae85
Simon Otter <simonotter@gmail.com>
2017-06-24 13:20:08 +0100

    Add Arrays unit test

--- Grades.Testss/Types/ReferenceTypeTests.cs
+++ Grades.Testss/Types/ReferenceTypeTests.cs
@@ -7,6 +7,22 @@ namespace Grades.Tests.Types
     public class TypeTests
     {
         [Test()]
+        public void UsingArrays()
+        {
+            float[] grades;
+            grades = new float[3];
+
+            AddGrades(grades);
+
+            Assert.AreEqual(89.1f, grades[1]);
+        }
+
+        private void AddGrades(float[] grades)
+        {
+            grades[1] = 89.1f;
+        }
+
+        [Test()]
         public void UppercaseString()
         {
             string name = "simon";
